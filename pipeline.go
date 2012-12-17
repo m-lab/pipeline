f7721c1bd743ee544a04692cd52369f5af88c9a5
Dominic Hamon <dominich@google.com>
2012-12-17 14:58:17 -0800

    Initial framework

---
+++ pipeline.go
@@ -0,0 +1,42 @@
+package pipeline
+
+import (
+  "fmt"
+  "github.com/gorilla/mux"
+  "net/http"
+)
+
+func init() {
+  r := mux.NewRouter()
+  r.HandleFunc("/", root).Methods("GET")
+  r.HandleFunc("/{tool}", tool).Methods("GET")
+  r.HandleFunc("/{tool}/{version}", tool_and_version)
+  http.Handle("/", r)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+  // TODO(dominic): Return information on pipeline and links to tool metrics.
+  fmt.Fprint(w, "Info on pipeline and links to tools will be here")
+}
+
+func tool(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  tool := vars["tool"]
+
+  // TODO(dominic): return metrics for tool
+  fmt.Fprintf(w, "Metrics for tool '%s' will be here", tool)
+}
+
+func tool_and_version(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  tool := vars["tool"]
+  version := vars["version"]
+
+  if r.Method == "GET" {
+    // TODO(dominic): return metrics for tool and version
+    fmt.Fprintf(w, "Metrics for tool '%s-%s' will be here", tool, version)
+  } else if r.Method == "POST" {
+    // TODO(dominic): get data from request and save to datastore for processing
+    w.WriteHeader(http.StatusFound)
+  }
+}
